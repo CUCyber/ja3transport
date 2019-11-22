d5a292a7b6c11287a96dbf6fa09d2ff305ff0c12
Max Harley <maxh@maxh.io>
2019-11-22 17:57:09 -0500

    Extract transport

--- transport.go
+++ transport.go
@@ -1,5 +1,78 @@
 package ja3transport
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	tls "github.com/refraction-networking/utls"
+)
+
+// GreasePlaceholder is a random value (well, kindof '0x?a?a) specified in a
+// random RFC.
+const GreasePlaceholder = 0x0a0a
+
+func ErrExtensionNotExist(e string) error {
+	return fmt.Errorf("Extension does not exist: %v\n", e)
+}
+
+// extMap maps extension values to the TLSExtension object associated with the
+// number. Some values are not put in here because they must be applied in a
+// special way. For example, "10" is the SupportedCurves extension which is also
+// used to calculate the JA3 signature. These JA3-dependent values are applied
+// after the instantiation of the map.
+var extMap = map[string]tls.TLSExtension{
+	"0": &tls.SNIExtension{},
+	"5": &tls.StatusRequestExtension{},
+	// These are applied later
+	// "10": &tls.SupportedCurvesExtension{...}
+	// "11": &tls.SupportedPointsExtension{...}
+	"13": &tls.SignatureAlgorithmsExtension{
+		SupportedSignatureAlgorithms: []tls.SignatureScheme{
+			tls.ECDSAWithP256AndSHA256,
+			tls.PSSWithSHA256,
+			tls.PKCS1WithSHA256,
+			tls.ECDSAWithP384AndSHA384,
+			tls.PSSWithSHA384,
+			tls.PKCS1WithSHA384,
+			tls.PSSWithSHA512,
+			tls.PKCS1WithSHA512,
+			tls.PKCS1WithSHA1,
+		},
+	},
+	"16": &tls.ALPNExtension{
+		AlpnProtocols: []string{"h2", "http/1.1"},
+	},
+	"18": &tls.SCTExtension{},
+	"21": &tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
+	"23": &tls.UtlsExtendedMasterSecretExtension{},
+	"27": &tls.FakeCertCompressionAlgsExtension{},
+	"28": &tls.FakeRecordSizeLimitExtension{},
+	"35": &tls.SessionTicketExtension{},
+	"43": &tls.SupportedVersionsExtension{[]uint16{
+		tls.GREASE_PLACEHOLDER,
+		tls.VersionTLS13,
+		tls.VersionTLS12,
+		tls.VersionTLS11,
+		tls.VersionTLS10}},
+	"44": &tls.CookieExtension{},
+	"45": &tls.PSKKeyExchangeModesExtension{[]uint8{
+		tls.PskModeDHE,
+	}},
+	"51": &tls.KeyShareExtension{[]tls.KeyShare{
+		{Group: tls.CurveID(GreasePlaceholder), Data: []byte{0}},
+		{Group: tls.X25519},
+	}},
+	"13172": &tls.NPNExtension{},
+	"65281": &tls.RenegotiationInfoExtension{
+		Renegotiation: tls.RenegotiateOnceAsClient,
+	},
+}
+
 // NewTransport creates an http.Transport which mocks the given JA3 signature when HTTPS is used
 func NewTransport(ja3 string) (*http.Transport, error) {
 	return NewTransportWithConfig(ja3, &tls.Config{})
@@ -31,4 +104,89 @@ func NewTransportWithConfig(ja3 string, config *tls.Config) (*http.Transport, er
 	}
 
 	return &http.Transport{DialTLS: dialtls}, nil
+}
+
+// stringToSpec creates a ClientHelloSpec based on a JA3 string
+func stringToSpec(ja3 string) (*tls.ClientHelloSpec, error) {
+	tokens := strings.Split(ja3, ",")
+
+	version := tokens[0]
+	ciphers := strings.Split(tokens[1], "-")
+	extensions := strings.Split(tokens[2], "-")
+	curves := strings.Split(tokens[3], "-")
+	if len(curves) == 1 && curves[0] == "" {
+		curves = []string{}
+	}
+	pointFormats := strings.Split(tokens[4], "-")
+	if len(pointFormats) == 1 && pointFormats[0] == "" {
+		pointFormats = []string{}
+	}
+
+	// parse curves
+	var targetCurves []tls.CurveID
+	for _, c := range curves {
+		cid, err := strconv.ParseUint(c, 10, 16)
+		if err != nil {
+			return nil, err
+		}
+		targetCurves = append(targetCurves, tls.CurveID(cid))
+	}
+	extMap["10"] = &tls.SupportedCurvesExtension{targetCurves}
+
+	// parse point formats
+	var targetPointFormats []byte
+	for _, p := range pointFormats {
+		pid, err := strconv.ParseUint(p, 10, 8)
+		if err != nil {
+			return nil, err
+		}
+		targetPointFormats = append(targetPointFormats, byte(pid))
+	}
+	extMap["11"] = &tls.SupportedPointsExtension{SupportedPoints: targetPointFormats}
+
+	// build extenions list
+	var exts []tls.TLSExtension
+	for _, e := range extensions {
+		te, ok := extMap[e]
+		if !ok {
+			return nil, ErrExtensionNotExist(e)
+		}
+		exts = append(exts, te)
+	}
+	// build SSLVersion
+	vid64, err := strconv.ParseUint(version, 10, 16)
+	if err != nil {
+		return nil, err
+	}
+	vid := uint16(vid64)
+
+	// build CipherSuites
+	var suites []uint16
+	for _, c := range ciphers {
+		cid, err := strconv.ParseUint(c, 10, 16)
+		if err != nil {
+			return nil, err
+		}
+		suites = append(suites, uint16(cid))
+	}
+
+	return &tls.ClientHelloSpec{
+		TLSVersMin:         vid,
+		TLSVersMax:         vid,
+		CipherSuites:       suites,
+		CompressionMethods: []byte{0},
+		Extensions:         exts,
+		GetSessionID:       sha256.Sum256,
+	}, nil
+}
+
+func urlToHost(target *url.URL) *url.URL {
+	if !strings.Contains(target.Host, ":") {
+		if target.Scheme == "http" {
+			target.Host = target.Host + ":80"
+		} else if target.Scheme == "https" {
+			target.Host = target.Host + ":443"
+		}
+	}
+	return target
 }
