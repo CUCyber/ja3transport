7ad1031b9381607cc9c49a03227832dc72939208
Max Harley <maxh@maxh.io>
2019-11-22 18:03:08 -0500

    Convert Chrome_Auto to variable

--- preset.go
+++ preset.go
@@ -5,7 +5,7 @@ type Browser struct {
 	UserAgent string
 }
 
-var Chrome_Auto Browser = Browser{
+var Chrome_Auto = Browser{
 	JA3:       "771,4865-4866-4867-49196-49195-49188-49187-49162-49161-52393-49200-49199-49192-49191-49172-49171-52392-157-156-61-60-53-47-49160-49170-10,65281-0-23-13-5-18-16-11-51-45-43-10-21,29-23-24-25,0",
 	UserAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1",
 }
