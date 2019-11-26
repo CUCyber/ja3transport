29073fc32b3da1cd09825fa78e55a93f8b23de34
Max Harley <maxh@maxh.io>
2019-11-25 21:24:45 -0500

    Create transport examples

--- ja3client_test.go
+++ ja3client_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	. "github.com/CUCyber/ja3transport"
+	tls "github.com/refraction-networking/utls"
 )
 
 const JA3Sig string = "771,4865-4866-4867-49196-49195-49188-49187-49162-49161-52393-49200-49199-49192-49191-49172-49171-52392-157-156-61-60-53-47-49160-49170-10,65281-0-23-13-5-18-16-11-51-45-43-10-21,29-23-24-25,0"
@@ -19,6 +20,24 @@ func ExampleNew() {
 
 func ExampleNewWithString() {
 	client, _ := NewWithString("771,4865-4866-4867-49196-49195-49188-49187-49162-49161-52393-49200-49199-49192-49191-49172-49171-52392-157-156-61-60-53-47-49160-49170-10,65281-0-23-13-5-18-16-11-51-45-43-10-21,29-23-24-25,0")
+	client.Get("https://ja3er.com/json")
+}
+
+func ExampleNewTransport() {
+	// Must import the `github.com/refraction-networking/utls` package to create the Config object.
+	tr, _ := NewTransport("771-61-60-53,0-23-15,29,23,24,0")
+	client := &http.Client{Transport: tr}
+	client.Get("https://ja3er.com/json")
+}
+
+func ExampleNewTransportWithConfig() {
+	// Must import the `github.com/refraction-networking/utls` package to create the Config object.
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	// Pass the config object to NewTransportWithConfig
+	tr, _ := NewTransportWithConfig("771-61-60-53,0-23-15,29,23,24,0", config)
+	client := &http.Client{Transport: tr}
 	client.Get("https://ja3er.com/json")
 }
 
@@ -43,6 +62,27 @@ func TestNew(t *testing.T) {
 }
 
 func TestNewTransport(t *testing.T) {
+	tr, err := NewTransport(JA3Sig)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://ja3er.com/json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dataB, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	data := string(dataB)
+	if !strings.Contains(data, "6fa3244afc6bb6f9fad207b6b52af26b") {
+		t.Fail()
+	}
+}
+
+func TestNewTransportWithConfig(t *testing.T) {
 	tr, err := NewTransport(JA3Sig)
 	if err != nil {
 		t.Fatal(err)
