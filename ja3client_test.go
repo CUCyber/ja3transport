41f5dd129ec5e60058353864b7c3913ba5fb129b
Elijah <elijahimp@gmail.com>
2023-01-23 07:21:45 +0200

    Update ja3client_test.go

--- ja3client_test.go
+++ ja3client_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	. "github.com/CUCyber/ja3transport"
-	tls "github.com/refraction-networking/utls"
+	tls "github.com/noooste/utls"
 )
 
 const DefaultJA3Sig string = "771,4865-4866-4867-49196-49195-49188-49187-49162-49161-52393-49200-49199-49192-49191-49172-49171-52392-157-156-61-60-53-47-49160-49170-10,65281-0-23-13-5-18-16-11-51-45-43-10-21,29-23-24-25,0"
